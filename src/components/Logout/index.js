0e781fc9259e0c745cd96ea6be2eabeb7f94e978
bijnamulanagaraj <bijnamula123@gmail.com>
2023-05-27 13:50:18 +0000

    solution

---
+++ src/components/Logout/index.js
@@ -0,0 +1,12 @@
+// Write your code here
+import './index.css'
+
+const Logout = props => {
+  const {onClickLogout} = props
+  return (
+    <button type="button" className="logout-button" onClick={onClickLogout}>
+      Logout
+    </button>
+  )
+}
+export default Logout
